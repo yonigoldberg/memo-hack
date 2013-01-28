eae9cf610fe02782dc9132f95dbb523bb9ef141d
Jonathan Goldberg <goldberg.jonathan@gmail.com>
2013-01-27 19:35:14 -0500

    support for the case when there are no questions

--- app/controllers/memory_controller.rb
+++ app/controllers/memory_controller.rb
@@ -6,7 +6,9 @@ class MemoryController < ApplicationController
 	def memory
 		@blogs = TumblrHelper.get_blog_list(current_user)
 
-		if (params.has_key?('qid'))
+		if no_questions
+			return
+		elsif (params.has_key?('qid'))
 			random_memory() unless @question = Question.find_by_id(params[:qid])
 		else
 			random_memory()
@@ -20,6 +22,11 @@ class MemoryController < ApplicationController
 	end
 
 	def question_form
+	end
+
+
+	def no_questions
+		Question.count == 0
 	end
 
 	def random_memory

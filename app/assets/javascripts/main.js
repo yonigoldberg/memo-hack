3671f2b3b1199f0ffb50ce06ab1892cbd2610222
Matan Zohar <matanzr@gmail.com>
2013-02-06 12:25:29 +0200

    location textbox with google maps api

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -51,6 +51,34 @@ function writePage(){
     $("#memoryLocation").change(postChanged);
     $("#mainTitleLarge").change(postChanged);
 
+    //google maps api location autocomplete
+    var input = document.getElementById("memoryLocation");
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    var infowindow = new google.maps.InfoWindow();
+
+     google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      infowindow.close();
+      // marker.setVisible(false);
+      input.className = '';
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        // Inform the user that the place was not found and return.
+        input.className = 'notfound';
+        return;
+      }
+   var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+      }
+
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    });
+
+
 }
 
 function mainPage(){

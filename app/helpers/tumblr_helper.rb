47eb03868c50e8fdc937cff44f3297979673b457
Matan Zohar <matanzr@gmail.com>
2013-02-10 16:53:29 +0200

    dry tumblr helper

--- app/helpers/tumblr_helper.rb
+++ app/helpers/tumblr_helper.rb
@@ -7,7 +7,7 @@ module TumblrHelper
 
   def self.save_post_as_draft(current_user, title, body, date, location)
   	access_token = current_user.prepare_tumblr_access_token
-    json = MultiJson.load(access_token.post("http://api.tumblr.com/v2/blog/#{current_user.tumblr_uid}.tumblr.com/post", 
+    json = MultiJson.load(access_token.post("http://api.tumblr.com/v2/blog/#{current_user.user_blog.blog_name}.tumblr.com/post", 
     	{
     		:type => "text",	
     		:state => "draft",
@@ -20,14 +20,17 @@ module TumblrHelper
 
   # specify type of posts to get - text, quote, link, answer, video, audio, photo, chat (none is any kind)
   def self.get_posts(current_user)
-  	access_token = current_user.prepare_tumblr_access_token
-  	json = MultiJson.load(access_token.get("http://api.tumblr.com/v2/blog/#{current_user.tumblr_uid}.tumblr.com/posts?api_key=#{ENV['TUMBLR_KEY']}").body.to_s)
+    json = self.tumblr_api_get_request_json(current_user, "posts")
   	json['response']['posts']
   end
 
   def self.get_drafts(current_user)
-  	access_token = current_user.prepare_tumblr_access_token
-  	json = MultiJson.load(access_token.get("http://api.tumblr.com/v2/blog/#{current_user.tumblr_uid}.tumblr.com/posts/draft?api_key=#{ENV['TUMBLR_KEY']}").body.to_s)
+  	json = self.tumblr_api_get_request_json(current_user, "posts/draft")
   	json['response']['posts']
+  end
+
+  def self.tumblr_api_get_request_json(current_user, api_url, parameters={})
+    access_token = current_user.prepare_tumblr_access_token
+    json = MultiJson.load(access_token.get("http://api.tumblr.com/v2/blog/#{current_user.user_blog.blog_name}.tumblr.com/#{api_url}?api_key=#{ENV['TUMBLR_KEY']}", parameters).body.to_s)
   end
 end
